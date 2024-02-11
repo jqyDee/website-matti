bca16a6f417a6cbc014c6651fa95ad1260e22485
jqyDee <40734461+jqyDee@users.noreply.github.com>
2024-02-11 12:05:46 -0700

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+function showSlideover(obj) {
+    document.getElementById("news-slideover").style.display = "flex";
+}
+
+function hideSlideover(obj) {
+    document.getElementById("news-slideover").style.display = "none";
+}
